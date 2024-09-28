41e8cb39f9cd32e288c64f89219301fa3cc2e4db
Ankit Kumar <49281815+ak3311g@users.noreply.github.com>
2024-09-28 15:26:18 +0530

    divided code

--- backend/error/logFile.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-export default function logFile(filename,message) {
-    const msg = JSON.stringify(message) + "\n";
-    //console.log(msg);
-    fs.appendFile(filename, msg, (err) => {
-        if (err) throw err;
-    });
-}
